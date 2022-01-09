4b3dea521f895107ef695716be6b2f9bb5d999aa
Christoph Siedentop <commits@siedentop.name>
2022-01-09 07:42:47 -0800

    Use the macos-perf library that I just published on crates.io

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -2,16 +2,16 @@
 use jeremy_kun_math_rust::{
     hilbert_matrix_vector_product, naive_matrix_vector_product, setup_hilbert, setup_inputs, Vector,
 };
+use macos_perf::compare_perf_counters;
 /// The original example from Jeremy Kun's Python code.
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
-use rust_macos_perf::compare_perf_counters;
 use std::time::{self, Instant};
 use timeit::timeit_loops;
 
 fn main() -> eyre::Result<()> {
     let mut rng = ChaCha8Rng::seed_from_u64(10);
-    rust_macos_perf::init()?;
+    macos_perf::init()?;
 
     let n: usize = 2usize.pow(11); // I observed a slowdown for the Hilbert code with '2^14'.
 
@@ -31,7 +31,7 @@ fn main() -> eyre::Result<()> {
         {  naive_matrix_vector_product(&A, &v, &mut output1, n); }
     };
 
-    let pc_naive = rust_macos_perf::timeit_loops! {timeit_count,
+    let pc_naive = macos_perf::timeit_loops! {timeit_count,
         {  naive_matrix_vector_product(&A, &v, &mut output1, n); }
     }?;
 
@@ -52,7 +52,7 @@ fn main() -> eyre::Result<()> {
         {hilbert_matrix_vector_product(&flattened_A,&v, &mut output2, &coordinate_iter);}
     };
 
-    let pc_hilbert = rust_macos_perf::timeit_loops! {timeit_count,
+    let pc_hilbert = macos_perf::timeit_loops! {timeit_count,
         {  hilbert_matrix_vector_product(&flattened_A, &v, &mut output2, &coordinate_iter); }
     }?;
 
--- src/bin/experiment.rs
+++ src/bin/experiment.rs
@@ -1,9 +1,9 @@
 use jeremy_kun_math_rust::{
     hilbert_matrix_vector_product, naive_matrix_vector_product, setup_hilbert, setup_inputs,
 };
+use macos_perf::PerformanceCounters;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
-use rust_macos_perf::PerformanceCounters;
 
 /// Experiment to compare Performance Counter with runtime.
 /// Is PerfCounter a linear regression for runtime?
@@ -20,7 +20,7 @@ struct Measurement {
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
-    rust_macos_perf::init()?;
+    macos_perf::init()?;
 
     let mut rng = ChaCha8Rng::seed_from_u64(10);
     let timeit_count = 20;
@@ -36,7 +36,7 @@ fn main() -> eyre::Result<()> {
             {  naive_matrix_vector_product(&A, &v, &mut output1, *n); }
         };
 
-        let pc_naive = rust_macos_perf::timeit_loops! {timeit_count,
+        let pc_naive = macos_perf::timeit_loops! {timeit_count,
             {  naive_matrix_vector_product(&A, &v, &mut output1, *n); }
         }?;
         print_row("naive", *n, total_n_seconds, pc_naive);
@@ -56,7 +56,7 @@ fn main() -> eyre::Result<()> {
             {hilbert_matrix_vector_product(&flattened_A,&v, &mut output, &coordinate_iter);}
         };
 
-        let pc_hilbert = rust_macos_perf::timeit_loops! {timeit_count,
+        let pc_hilbert = macos_perf::timeit_loops! {timeit_count,
             {  hilbert_matrix_vector_product(&flattened_A, &v, &mut output, &coordinate_iter); }
         }?;
         print_row("hilbert", n, total_h_seconds, pc_hilbert);
